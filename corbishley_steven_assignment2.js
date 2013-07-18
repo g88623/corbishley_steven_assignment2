85d6655e8d50393b6a5801b85e0f12ef6d14b012
g88623 <steven243@fullsail.edu>
2013-07-18 01:55:04 -0400

    Changed JS file name, updated JS path in HTML, and added assignment outline.

---
+++ corbishley_steven_assignment2.js
@@ -0,0 +1,47 @@
+// Steven Corbishley
+// SDI 1307
+// Project 2
+// My SDI Project 2 JavaScript file.
+
+// My Variables
+	// String
+	// Number
+	// Array
+
+// My Procedure
+	// Procedure takes an Argument.
+	// My Argument Conditional
+		// If True, Output 1
+		// If False, Output 2
+
+// My Boolean Function
+	// Boolean Function takes in two Arguments.
+	// Compares the two arguments.
+		// If True, Output 1
+		// If False, Output 2
+		// Return Boolean
+
+// My Number Function
+	// Number Function takes a Number argument.
+	// Includes local variables.
+	// Enters While Loop.
+		// If True, Perform Math, Output, and continue loop.
+		// If False, finish loop and return Number.
+
+// My String Function
+	// String Function takes in two String arguments.
+	// Includes local variables.
+	// Concatenates Strings.
+	// Returns String.
+
+// My Array Function
+	// Array Function takes in an Array argument AND a Number argument.
+	// Includes local variables.
+	// Enters For Loop
+		// If True, performs Math, Outputs, and continues For Loop.
+		// If False, returns an Array.
+
+
+// My Returned Values
+
+// My Output
